678d36fa77695be4cfc38cf2b36b5b20be5cc2e7
Michael Nguyen <michael_n35@yahoo.com>
2025-04-25 02:44:37 -0700

    Completed some of extract next links

--- scraper.py
+++ scraper.py
@@ -1,11 +1,11 @@
 import re
 import requests
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urldefrag, urlunparse
 from bs4 import BeautifulSoup
 
-def scraper(url, resp):
-    links = extract_next_links(url, resp)
-    return [link for link in links if is_valid(link)]
+# def scraper(url, resp):
+#     links = extract_next_links(url, resp)
+#     return [link for link in links if is_valid(link)]
 
 def extract_next_links(url, resp):
     # Implementation required.
@@ -17,7 +17,38 @@ def extract_next_links(url, resp):
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    return list()
+
+
+    # All url/path that we have to verify
+    valid_url = [
+        "ics.uci.edu",
+        "cs.uci.edu",
+        "informatics.uci.edu",
+        "stat.uci.edu",
+    ]
+    valid_url_path = "today.uci.edu/department/information_computer_sciences/"
+
+    # Store all valid links here
+    links = []
+
+    # Parse content with lxml cause fastest, check if have href, parse hostname, parse for no scheme
+    content = BeautifulSoup(resp, "lxml")
+    for link in content.find_all("a", href=True):
+        original_url = link.get("href")
+        parsed_url = urlparse(original_url)
+        original_url_no_scheme = urlunparse(("", parsed_url.netloc, parsed_url.path, parsed_url.params, parsed_url.query, parsed_url.fragment))
+        if original_url_no_scheme.startswith("//"):
+            original_url_no_scheme = original_url_no_scheme[2:]
+
+        # if valid with links, defragment it (it returns a tuple)
+        if parsed_url.hostname:
+            for url in valid_url:
+                if parsed_url.hostname.endswith(url) or original_url_no_scheme.startswith(valid_url_path):
+                    link_defragged, dummy = urldefrag(original_url)
+                    links.append(link_defragged)
+                    break
+
+    return links
 
 def is_valid(url):
     # Decide whether to crawl this url or not. 
@@ -40,3 +71,38 @@ def is_valid(url):
     except TypeError:
         print ("TypeError for ", parsed)
         raise
+
+
+if __name__ == "__main__":
+    html_doc = """
+    <html><head><title>The Dormouse's story</title></head>
+    <body>
+    <p class="title"><b>The Dormouse's story</b></p>
+
+    <p class="story">Once upon a time there were three little sisters; and their names were
+    <a href="http://example.com/elsie#bruh" class="sister" id="link1">Elsie</a>,
+    <a href="https://ics.uci.edu/facts-figures/ics-mission-history/" class="sister" id="link2">Lacie</a> and
+    <a href="https://cs.ics.uci.edu/" class="sister" id="link3">Tillie</a>;
+    <a href="https://tutoring.ics.uci.edu/resources/" class="sister" id="link3">Tillie</a>;
+    <a href="https://today.uci.edu/department/information_computer_sciences/" class="sister" id="link3">Tillie</a>;
+    <a href="https://cs.ics.uci.edu/" class="sisterdafdsafdsa" id="link3">Tilliefdsafdsa</a>;
+    and they lived at the bottom of a well.</p>
+
+    <p class="story">...</p>
+    """
+    links = extract_next_links("test", html_doc) # PRETEND THAT RESP = THE EXAMPLE HTML DOC, CHANGE IT BACK WHENEVBER WITH RESP.CONTENT STUFFS
+
+    # TODO EXTRACTNEXTLINKS
+    # Detect and avoid dead URLs that return a 200 status but no data (click here to see what the different HTTP status codes mean
+    # Crawl all pages with high textual information content (CHECK IF CONTENT GOOD, IF GOOD THEN PARSE AND GET LINKS)
+    # Detect and avoid crawling very large files, especially if they have low information value (SAME AS ABOVE IF LOW INFO VALUE/LARGE SIZE DONT BOTHER)
+    # Detect and avoid sets of similar pages with no information
+    # Honor the politeness delay for each site
+
+    # TODO ISVALID
+    # Detect and avoid infinite traps
+
+
+    valids = [link for link in links if is_valid(link)]
+    for l in valids:
+        print(l)
